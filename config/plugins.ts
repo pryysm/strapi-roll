48583df3bf742f5976fbd4c39a941d7cd2994246
renomureza <renomureza@gmail.com>
2024-02-24 20:30:07 +0700

    add cloudinary plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,11 +1,27 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 export default ({ env }) => ({
   // ...
-  'users-permissions': {
+  "users-permissions": {
     config: {
       jwt: {
-        jwtSecret: env('STRAPI_JWT_SECRET') || crypto.randomBytes(16).toString('base64')
+        jwtSecret:
+          env("STRAPI_JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
+      },
+    },
+  },
+  upload: {
+    enabled: env("NODE_ENV") === "production",
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
